aa4405e700df6ac7aa55cf7de5b58ce4cb083a9c
swapna9eureka <swapna@eurekamobilead.com>
2018-03-23 16:54:04 +0530

    change

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this is test change
+        //this is test
         setContentView(R.layout.activity_main);
     }
 }
